63e4d6bdd0c745a6d14fdaae7a97bbe3ff214f6c
João Vitor Witt <joaovitorwitt2004@gmail.com>
2024-08-17 11:50:38 -0300

    Python ===> C

--- core/scene.py
+++
@@ -1,7 +0,0 @@
-from core.object3D import Object3D
-
-
-class Scene(Object3D):
-
-    def __init__(self):
-        super().__init__()
--- geometry/hexagon.py
+++
@@ -1,5 +0,0 @@
-from geometry.polygonGeometry import PolygonGeometry
-
-class HexagonGeometry(PolygonGeometry):
-    def __init__(self, radius=1):
-        super().__init__(sides=6, radius=radius)
